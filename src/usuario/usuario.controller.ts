2fb4e7f4de8dbccd6111d0e139f3e20fc8d3d891
Gianine DataType <gianine.rosa@datatype.com.br>
2023-02-11 12:35:51 -0300

    correction service and controller

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,53 +1,6 @@
 import { Controller, NotFoundException, BadRequestException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { UpdateUserDto } from './dtos/update-user.dto';
-import { Usuario } from './interface/usuario.interface';
 
 @Controller('api/usuario')
 export class UsuarioController {
-  constructor(@InjectModel('Usuario') private readonly usuarioModel: Model<Usuario>){}
-
-  async createUser(createUserDto: CreateUserDto): Promise<Usuario>{
-    const { email } = createUserDto;
-    const userFinding = this.usuarioModel.find({email}).exec();
-
-    if(userFinding){
-      throw new BadRequestException(`Usuário com email: ${email} já cadastrado`);
-    }
-    const newUser = new this.usuarioModel(createUserDto);
-    return await newUser.save();
-  }
-
-  async getAllUser(): Promise<Array<Usuario>>{
-    return await this.usuarioModel.find().exec();
-  }
-
-  async getUniqueUser(_id: string): Promise<Usuario>{
-    return await this.validadeExistUser(_id);
-  }
-
-  async updateUser(updateUserDto: UpdateUserDto, _id: string): Promise<void>{
-    await this.validadeExistUser(_id);
-
-    await this.usuarioModel.findOneAndUpdate({_id},{$set: updateUserDto}).exec();
-
-  }
-
-  async deleteUser(_id: string): Promise<void>{ // Promise<any>
-    await this.validadeExistUser(_id);
-    await this.usuarioModel.deleteOne({_id}).exec();
-  }
-
-  private async validadeExistUser(_id: string): Promise<Usuario>{
-    const userFinding = await this.usuarioModel.findOne({_id}).exec();
-
-    if(!userFinding){
-      throw new NotFoundException(`Usuário não foi encontrado`);
-    };
-
-    return userFinding;
-  }
 
 }
